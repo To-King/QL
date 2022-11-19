061a991a87278b78e31af1ecef8e608bbafcba50
To-King <ugg999999@gmail.com>
2022-11-19 14:03:12 +0800

    init qiandao

---
+++ web/handlers/about.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# vim: set et sw=4 ts=4 sts=4 ff=unix fenc=utf8:
+# Author: Binux<i@binux.me>
+#         http://binux.me
+# Created on 2014-08-08 21:06:02
+
+from .base import *
+import os
+
+class AboutHandler(BaseHandler):
+    @tornado.web.addslash
+    async def get(self):
+        with open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'tpl','about.html'), 'r',encoding='utf-8') as f:
+            html_content = f.read()
+
+        await self.finish(html_content)
+
+handlers = [
+        ('/about/?', AboutHandler),
+        ]
